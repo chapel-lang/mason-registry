a01d59596b1cedae760f9879fbb4814a1beafcf3
oplambeck <owen.plambeck@gmail.com>
2019-07-08 09:58:55 -0700

    Grabbing chapel from git and making it
    Added Test to make chapel
    
    Added setting the env
    
    Added CHPL_REGEXP to re2
    
    Added 'export' to set the REGEXP
    
    Printing out chpl env to see if reg exp worked
    
    Started working on chpl script to check format of tomls
    
    Progress Save
    
    Added a test that checks the content of the toml files
    
    -Checks to see if the fields are not empty
    -checks to see if the source is a valid git remote
    -added new test to the travis file
    
    Moved make chapel into same command as chpl compliation
    
    Removed the excutable so the test can compile the chpl test
    
    Added 'cd' command to the make chapel script
    
    Added language and compiler to travis
    
    Reordering Travis yml
    
    Fixed Travis File
    
    Fixing trav
    
    Added get stage
    
    Added "/"
    
    Trying this
    
    Using MASON_HOME
    
    Changed path to CHPL_HOME
    
    Try this travis
    
    added REGEXP=re2
    
    added varibles
    
    added path
    
    added manpath
    
    test of the chpl --help
    
    Added make install
    
    with help output
    
    simplified the process
    
    build in parallel
    
    using quickstart
    
    test
    
    with ls
    
    make
    
    cd chapel
    
    added configure
    
    checking in preivous stage
    
    trying again
    
    added example test
    
    test
    
    push
    
    added configure back
    
    added chpl to first command
    
    switched to smoke test
    
    stupid mistake
    
    fixed path to smoke test
    
    ls
    
    fixed path
    
    another try
    
    cd
    
    Grabbing smoke test from chapel/.travis.yml
    
    Formatting
    
    Changed Paths
    
    Path
    
    Fixed tests to run in order
    
    Removed "ls"
    
    More fixes
    
    REmoved bash
    
    added '.'
    
    another try
    
    fixed stage names
    
    added ./
    
    switched call
    
    turned matrix to jobs
    
    moved stages up
    
    changed depth
    
    another try
    
    regexp
    
    no actaully running smoketest
    
    path fix
    
    path fix
    
    Trying to figure out where chapel is
    
    trying something new
    
    faster make
    
    Chapel is being made and passes make check within travis CI
    
    test
    
    typo
    
    path
    
    update
    
    working on implementing check
    
    fixed
    
    back to old
    
    moved
    
    fixed syntax
    
    syntax
    
     getting msot recent package
    
    more fixes
    
    finding source
    
    test
    
    took away
    
    again
    
    had a character
    
    again
    
    a
    
    b
    
    a
    
    again
    
    a
    
    test
    
    abc
    
    abc
    
    1
    
    2
    
    2
    
    3
    
    4
    
    5
    
    6
    
    10
    
    11
    
    56
    
    57
    
    5
    
    77
    
    1
    
    6
    
    2
    
    10
    
    2
    
    3
    
    avs
    
    1
    
    a
    
    ab
    
    abc
    
    forgot a $
    
    a
    
    a
    
    a
    
    feel good about this one
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    al
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    abc
    
    a
    
    apple
    
    l
    
    apple2
    
    a
    
    printing out the result of grep
    
    1
    
    123
    
    da
    
    all
    
    a
    
    a
    
    a
    
    a
    
    a
    
    a
    
    echo
    
    a
    
    work
    
    removed old tests
    
    a

---
+++ util/travisScript.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#Clones master of chapel and quickstarts with CHPL_REGEXP=re2
+git clone --depth=50 --branch=master https://github.com/chapel-lang/chapel.git
+cd chapel
+source util/quickstart/setchplenv.bash
+export CHPL_REGEXP=re2
+make -j 4
+
+#Runs a make check and makes mason
+make check
+cd tools/mason
+make install
+cd ../../../
+pwd
+
+# Parses the last merge commit, getting the most recent package added to the registry
+package=$(git log -m -1 --name-only --pretty="format:")
+pwd
+end=".end"
+path="$package$end"
+cd $(dirname $path)
+# grabs the source from the toml
+echo $package
+pwd
+FILE=$package
+basename "$FILE"
+f="$(basename -- $FILE)"
+echo "$f"
+source="$(grep source "$f" | cut -d= -f2)"
+echo $source
+#strips the quotes off of the source
+fixed=$(echo "$source" | tr -d '"')
+#clones the source
+echo $fixed
+git clone $fixed newPackage
+cd newPackage
+#runs mason publish --check --travis on the package
+mason publish --check --travis
+
+
+
