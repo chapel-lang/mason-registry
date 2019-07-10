639de99aea8ad1d423cc59ca38f4603bd87e42de
oplambeck <owen.plambeck@gmail.com>
2019-07-10 15:18:51 -0700

    using quickstart

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -5,8 +5,5 @@ git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel
 cd chapel
 source util/quickstart/setchplenv.bash
 make -j 4 CHPL_REGEXP=re2
-CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
-export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
-export MANPATH="$MANPATH":"$CHPL_HOME"/man
 make install
 
