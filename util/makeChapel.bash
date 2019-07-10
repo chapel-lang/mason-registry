765894d3cfbb03816473c884f0ab643d1a9cc3fe
oplambeck <owen.plambeck@gmail.com>
2019-07-10 15:15:10 -0700

    build in parallel

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -4,7 +4,7 @@ cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
 cd chapel
 source util/quickstart/setchplenv.bash
-make CHPL_REGEXP=re2
+make -j 4 CHPL_REGEXP=re2
 CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
 export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
 export MANPATH="$MANPATH":"$CHPL_HOME"/man
