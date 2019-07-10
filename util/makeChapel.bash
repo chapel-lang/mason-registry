cc0d31ba56011d36962cd69213e9522b62658d1d
oplambeck <owen.plambeck@gmail.com>
2019-07-10 13:34:09 -0700

    Reordering Travis yml

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -3,8 +3,4 @@
 cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
 cd chapel
-source util/quickstart/setchplenv.bash
-export CHPL_REGEXP=re2
-make -j 4
-make check
-printchplenv
+
