a23977a679f25b45e2b75503664e223f3379309d
oplambeck <owen.plambeck@gmail.com>
2019-07-10 14:34:42 -0700

    added varibles

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -2,6 +2,6 @@
 
 cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
-./chapel/util/buildRelease/smokeTest
+./chapel/util/buildRelease/smokeTest CHPL_SMOKE_SKIP_DOC=true NIGHTLY_TEST_SETTING=true
 CHPL_REGEXP=re2
 
