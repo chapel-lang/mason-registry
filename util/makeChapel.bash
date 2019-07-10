59c7541d0978486a622ea060b4c0e96f040d2e1f
oplambeck <owen.plambeck@gmail.com>
2019-07-10 15:11:46 -0700

    simplified the process

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -2,10 +2,11 @@
 
 cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
-./chapel/util/buildRelease/smokeTest CHPL_SMOKE_SKIP_DOC=true NIGHTLY_TEST_SETTING=true CHPL_REGEXP=re2
 cd chapel
+source util/quickstart/setchplenv.bash
+make CHPL_REGEXP=re2
 CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
 export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
 export MANPATH="$MANPATH":"$CHPL_HOME"/man
 make install
-chpl --help
+
