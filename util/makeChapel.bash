fc7fcf1d0423c77d445bd00e73072a126e08db58
oplambeck <owen.plambeck@gmail.com>
2019-07-10 14:56:23 -0700

    test of the chpl --help

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -6,3 +6,4 @@ git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel
 CHPL_BIN_SUBDIR=`"$CHPL_HOME"/util/chplenv/chpl_bin_subdir.py`
 export PATH="$PATH":"$CHPL_HOME/bin/$CHPL_BIN_SUBDIR"
 export MANPATH="$MANPATH":"$CHPL_HOME"/man
+chpl --help
