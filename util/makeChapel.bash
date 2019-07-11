d62410912357f514eab09f2e7e17b1449470bd84
oplambeck <owen.plambeck@gmail.com>
2019-07-11 14:17:44 -0700

    test

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -3,7 +3,6 @@
 cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
 cd chapel
-./configure
 source util/quickstart/setchplenv.bash
 make -j 4 CHPL_REGEXP=re2
 sudo make install
