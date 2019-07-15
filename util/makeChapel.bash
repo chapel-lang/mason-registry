fefd57d08c2c4c4f82fc984e629c156562f59770
oplambeck <owen.plambeck@gmail.com>
2019-07-15 08:14:39 -0700

    switched to smoke test

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -2,10 +2,3 @@
 
 cd
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
-cd chapel
-source util/quickstart/setchplenv.bash
-./configure
-export CHPL_REGEXP=re2
-make -j 4
-sudo make install
-chpl -o hello examples/hello.chpl
