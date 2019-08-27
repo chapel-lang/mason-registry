a36e15a82ffe99b04390995c825705ae77ce6843
oplambeck <owen.plambeck@gmail.com>
2019-08-27 10:05:02 -0700

    faster make

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -4,7 +4,7 @@ git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel
 cd chapel
 source util/quickstart/setchplenv.bash
 export CHPL_REGEXP=re2
-make
+make -j 4
 make check
 
 
