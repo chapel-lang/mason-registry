14dd3eda47cac95e43d5edaddc2d8af9a93328a2
oplambeck <owen.plambeck@gmail.com>
2019-08-27 09:59:19 -0700

    Trying to figure out where chapel is

--- getChapel.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-echo 'git clone --depth=10 --branch=release/1.19 https://github.com/chapel-lang/chapel.git'
-cd chapel
-echo 'source util/quickstart/setchplenv.bash'
-echo 'make'
--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -2,4 +2,5 @@
 
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
 export CHPL_REGEXP=re2
+ls
 
