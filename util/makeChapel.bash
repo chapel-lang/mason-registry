c39d75e871b1be58b56148cf51e289a8529a9718
oplambeck <owen.plambeck@gmail.com>
2019-08-27 10:02:12 -0700

    trying something new

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 git clone --depth=50 --branch=release/1.19 https://github.com/chapel-lang/chapel.git
+cd chapel
+source util/quickstart/setchplenv.bash
 export CHPL_REGEXP=re2
-ls
+make
+make check
+
+
 
