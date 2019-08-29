a6bd9035a6c7b0c6e603a5762b6ec98587a822a1
oplambeck <owen.plambeck@gmail.com>
2019-08-29 14:28:53 -0700

    2

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -15,8 +15,9 @@ package=$(git log -m -1 --name-only --pretty="format:")
 touch source
 awk -F= '/source/ { print $2 > "source" }' $package
 packageReg=$(cat source)
-git clone $packageReg newestPackage
-cd newestPackage
+git clone $packageReg as 'newPackage' 
+cd package
+cd newPackage
 mason publish --check --travis
 
 
