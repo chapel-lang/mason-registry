1988162460cf2a65bb2c34f7a92b5a7077f84aba
oplambeck <owen.plambeck@gmail.com>
2019-08-29 15:12:33 -0700

    a

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+#Clones master of chapel and quickstarts with CHPL_REGEXP=re2
 git clone --depth=50 --branch=master https://github.com/chapel-lang/chapel.git
 cd chapel
 source util/quickstart/setchplenv.bash
 export CHPL_REGEXP=re2
 make -j 4
+
+#Runs a make check and makes mason
 make check
 cd tools/mason
 make install
@@ -15,6 +18,7 @@ package=$(git log -m -1 --name-only --pretty="format:")
 echo $package
 source=$(grep source $package | cut -d= -f2)
 echo $source
+git clone https://github.com/marcoscleison/chapel-gnuplot.git
 git clone $source newPackage
 cd newPackage
 mason publish --check --travis
