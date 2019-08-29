5eaeb09a0e58326b11dd82a3edd0d50339abef98
oplambeck <owen.plambeck@gmail.com>
2019-08-29 14:58:22 -0700

    a

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -15,10 +15,7 @@ package=$(git log -m -1 --name-only --pretty="format:")
 echo $package
 source=$(grep source $package | cut -d= -f2)
 echo $source
-packageReg=$(cat $source)
-cd $package
-echo $packageReg
-git clone $packageReg newPackage
+git clone $source newPackage
 cd newPackage
 mason publish --check --travis
 
