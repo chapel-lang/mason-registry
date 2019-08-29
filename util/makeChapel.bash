b8108a7ca3eb6a1362da1349e42cbff1dd96b6f6
oplambeck <owen.plambeck@gmail.com>
2019-08-29 13:01:35 -0700

    abc

--- util/makeChapel.bash
+++ util/makeChapel.bash
@@ -12,8 +12,8 @@ cd ../../..
 
 
 package=$(git log -m -1 --name-only --pretty="format:")
-source=$(awk -F= '/source/ { print $2 }' $package)
-echo $source
-git clone $source
+awk -F= '/source/ { print $2 }' $package)
+
+git clone $2
 
 
