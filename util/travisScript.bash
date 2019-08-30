7d95698f9346ef9ddb6a2d933bbdc61ee2f73dc7
oplambeck <owen.plambeck@gmail.com>
2019-08-30 13:37:19 -0700

    Cleaned up the formatting and adressed some reviews

--- util/travisScript.bash
+++ util/travisScript.bash
@@ -1,42 +1,54 @@
 #!/usr/bin/env bash
 
-#Clones master of chapel and quickstarts with CHPL_REGEXP=re2
-git clone --depth=50 --branch=master https://github.com/chapel-lang/chapel.git
+# Clones master of chapel and quickstarts with CHPL_REGEXP=re2
+git clone --depth=1 --branch=master https://github.com/chapel-lang/chapel.git
 cd chapel
-source util/quickstart/setchplenv.bash
-export CHPL_REGEXP=re2
-make -j 4
 
-#Runs a make check and makes mason
-make check
-cd tools/mason
-make install
-cd ../../../
-pwd
+buildChapel {
+    cd chapel
+    source util/quickstart/setchplenv.bash
+    export CHPL_REGEXP=re2
+    make -j 4
+}
+
+# Runs a make check, and if it passes then makes mason
+makeCheckAndMason {
+    make check
+    output=$?
+    if [ '$output' -eq 0 ]; then
+        make mason
+    else
+        exit 1
+    fi
+}
+
 
 # Parses the last merge commit, getting the most recent package added to the registry
-package=$(git log -m -1 --name-only --pretty="format:")
-pwd
-end=".end"
-path="$package$end"
-cd $(dirname $path)
-# grabs the source from the toml
-echo $package
-pwd
-FILE=$package
-basename "$FILE"
-f="$(basename -- $FILE)"
-echo "$f"
-source="$(grep source "$f" | cut -d= -f2)"
-echo $source
-#strips the quotes off of the source
-fixed=$(echo "$source" | tr -d '"')
-#clones the source
-echo $fixed
-git clone $fixed newPackage
-cd newPackage
-#runs mason publish --check --travis on the package
+checkPackage {
+    package=$(git log -m -1 --name-only --pretty="format:")
+    end=".end"
+    path="$package$end"
+    cd $(dirname $path)
+
+    # Parses the source from the toml
+    FILE=$package
+    basename "$FILE"
+    f="$(basename -- $FILE)"
+    source="$(grep source "$f" | cut -d= -f2)"
+
+    # Strips the quotes off of the source
+    fixed=$(echo "$source" | tr -d '"')
+
+    # Clones the source
+    git clone $fixed newPackage
+    cd newPackage
+}
+
+buildChapel
+
+makeCheckAndMason
+
+checkPackage
+
 mason publish --ci-check
-
-
-
+exit $?
