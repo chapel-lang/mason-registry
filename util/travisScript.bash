6a70145af6aad5dab3f25ee075b25505249bbb96
oplambeck <owen.plambeck@gmail.com>
2019-08-30 14:12:36 -0700

    Fixed function format

--- util/travisScript.bash
+++ util/travisScript.bash
@@ -4,24 +4,29 @@
 git clone --depth=1 --branch=master https://github.com/chapel-lang/chapel.git
 cd chapel
 
-buildChapel {
+buildChapel()
+{
   cd chapel
   source util/quickstart/setchplenv.bash
   export CHPL_REGEXP=re2
-  make }
+  make
+}
 
 # Runs a make check, and if it passes then makes mason
-makeCheckAndMason {
+makeCheckAndMason()
+{
   make check
   output=$?
   if [ '$output' -eq 0 ]; then
     make mason
   else
     exit 1
-  fi }
+  fi
+}
 
 # Parses the last merge commit, getting the most recent package added to the registry
-checkPackage {
+checkPackage()
+{
   package=$(git log -m -1 --name-only --pretty="format:")
   end=".end"
   path="$package$end"
@@ -38,7 +43,8 @@ checkPackage {
 
   # Clones the source
   git clone $fixed newPackage
-  cd newPackage }
+  cd newPackage
+}
 
 buildChapel
 
