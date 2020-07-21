5ee04d2e57b0b5129fdd1bd9c816180854e64089
ankush.bhardwaj0@gmail.com <ankush.bhardwaj0@gmail.com>
2020-07-21 19:27:05 +0530

    refactored scripts and disabled onpush workflow

--- util/searchRankScript.bash
+++ util/searchRankScript.bash
@@ -1,58 +1,63 @@
 #!/usr/bin/env bash
 # Clone the package to be published from latest commit
 package=$(git diff --name-only HEAD HEAD~1)
-end=".end"
+echo "[LOG]: Package path from git command = ${package}"
 
 # check for TOML file
-if [[ $package != *".toml"* ]]; then
+if [[ $(basename -- $package) != *".toml"* ]]; then
   echo "Not TOML FILE"
   exit 0
 fi
-path="$package$end"
-cd "$(dirname "$path")" || exit 1
+
+path="$package"
+echo "[LOG]: path = $(dirname ${path})"
 FILE=$package
-basename "$FILE"
 f="$(basename -- "$FILE")"
-source="$(grep source "$f"| cut -d= -f2)"
-fixed=$(echo "$source" | tr -d '"')
-name="$(grep name "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
-version="$(grep version "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
-git clone "$fixed" newPackage
-cd newPackage || exit 1
+echo "[LOG]: Package TOML file = ${f}"
+source="$(grep source "$path"| cut -d= -f2)"
+fixed=$(echo "$source" 2> /dev/null | tr -d '"')
+echo "[LOG]: Package url = ${fixed}"
+name="$(grep name "$path" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
+echo "[LOG]: Package name = ${name}"
+version="$(grep version "$path" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
+echo "[LOG]: Version = ${version}"
+git clone "$fixed" "$(dirname ${path})/newPackage"
+packagePath="$(dirname ${path})/newPackage"
+echo "$name cloned succesfully at $packagePath"
 
 # Perform a series of check on the package and award points
-mason=./Mason.toml
-testDir=./test/
-exampleDir=./example/
-srcDir=./src/
+mason=$packagePath/Mason.toml
+testDir=$packagePath/test/
+exampleDir=$packagePath/example/
+srcDir=$packagePath/src/
 score=0
-countForREADME=$(ls -l ./README.* 2>/dev/null | wc -l)
+countForREADME=$(ls -l $packagePath/README.* 2>/dev/null | wc -l)
 if [ "$countForREADME" != 0 ]
 then
-  echo "README found"
+  echo "README found in package"
   score=$((score+1))
 fi
-countForModuleFile=$(ls -l ./src/*.chpl 2>/dev/null | wc -l)
+countForModuleFile=$(ls -l $packagePath/src/*.chpl 2>/dev/null | wc -l)
 if [ "$countForModuleFile" != 0 ]
 then
-  echo "ModuleFile found"
+  echo "ModuleFile found in package"
   score=$((score+1))
 fi
-[[ -f "$mason" ]] && score=$((score+1)) && echo "Mason.toml found"
-[[ -d "$testDir" ]] && score=$((score+1)) && echo "test dir found"
-[[ -d "$exampleDir" ]] && score=$((score+1)) && echo "example dir found"
-[[ -d "$srcDir" ]] && score=$((score+1)) && echo "src dir found"
+[[ -f "$mason" ]] && score=$((score+1)) && echo "Mason.toml found in package"
+[[ -d "$testDir" ]] && score=$((score+1)) && echo "test dir found in package"
+[[ -d "$exampleDir" ]] && score=$((score+1)) && echo "example dir found in package"
+[[ -d "$srcDir" ]] && score=$((score+1)) && echo "src dir found in package"
 
 # points for number of examples 
-countForExamples=$(ls -l ./example/*.chpl 2>/dev/null | wc -l)
+countForExamples=$(ls -l $exampleDir/*.chpl 2>/dev/null | wc -l)
+echo "Found $countForExamples examples in package."
 score=$((score+countForExamples))
 
 # points for number of tests
-countForTests=$(ls -l ./test/*.chpl 2>/dev/null | wc -l)
+countForTests=$(ls -l $testDir/*.chpl 2>/dev/null | wc -l)
+echo "Found $countForTests tests in package."
 score=$((score+countForTests))
-echo "$score"
-
-cd ../../../ || exit 1
+echo "Total score generated for $name = $score"
 
 # check if package name and version already exists 
 if grep "$name.\"$version\"" cache.toml
@@ -64,5 +69,5 @@ fi
 # append package score to TOML cache file
 echo "[$name.\"$version\"]" >> cache.toml
 echo "score = $score" >> cache.toml
-echo "Wrote $name=$score to cache.toml"
+echo "Wrote '$name = $score' to cache.toml"
 
