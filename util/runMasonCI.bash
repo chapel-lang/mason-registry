b30ae775524b4cd0f95ce9147338d5097f1c76c0
Jeremiah Corrado <jeremiah.corrado@hpe.com>
2024-05-08 08:25:27 -0600

    remove ssh->html step of CI setup
    Signed-off-by: Jeremiah Corrado <jeremiah.corrado@hpe.com>

--- util/runMasonCI.bash
+++ util/runMasonCI.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clones master of chapel and quickstarts with CHPL_REGEXP=re2
-git clone --depth=1 --branch=main https://github.com/chapel-lang/chapel.git
+git clone --depth=1 --branch=main git@github.com:chapel-lang/chapel.git
 
 buildChapel () {
   cd chapel || exit 1
@@ -57,4 +57,3 @@ mason update
 
 mason publish --ci-check
 exit $?
-
