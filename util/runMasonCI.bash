29291ad422d050c519f2afd8ed385e48c302699e
Jade Abraham <jade.abraham@hpe.com>
2026-03-10 16:08:28 -0700

    fix the CI, since HEAD is not visible in the container
    Signed-off-by: Jade Abraham <jade.abraham@hpe.com>

--- util/runMasonCI.bash
+++ util/runMasonCI.bash
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 
-# Parses the last merge commit, getting the most recent package added to the registry
 checkPackage () {
-  package=$(git diff --name-only HEAD HEAD~1 | grep -E '.*\.toml')
-  end=".end"
-  path="$package$end"
-  cd "$(dirname "$path")" || exit 1
+  package=$1
+  if [ -z "$package" ]; then
+    echo "No package path provided. Exiting."
+    exit 1
+  fi
+  if [ ! -f "$package" ]; then
+    echo "Provided package path does not exist: $package. Exiting."
+    exit 1
+  fi
   echo "package detected from git diff: ${package}"
   echo "package path: ${path}"
 
   # Parses the source from the toml
-  FILE=$package
-  basename "$FILE"
-  f="$(basename -- "$FILE")"
-  source="$(grep source "$f" | cut -d= -f2)"
+  source="$(grep source "$package" | cut -d= -f2)"
   echo "source value: $source"
   # Strips the quotes off of the source
   fixed=$(echo "$source" | tr -d '"' | awk '{$1=$1};1')
@@ -25,7 +26,7 @@ checkPackage () {
   cd newPackage || exit 1
 }
 
-checkPackage
+checkPackage "$1"
 
 # adds mason-registry to MASON_HOME
 mason update
